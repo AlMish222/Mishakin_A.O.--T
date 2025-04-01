61ce7896a8d7132422a6fe184aea24d3c3ce9448
NEGOTIVE <negotive8@yandex.ru>
2025-04-01 19:28:36 +0300

    HomeWork 6 commit number 2

--- app/src/main/java/com/example/dz_1/ItemsAdapter.kt
+++ app/src/main/java/com/example/dz_1/ItemsAdapter.kt
@@ -1,37 +1,57 @@
 package com.example.dz_1
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import classes.library.LibraryItems
 import com.example.dz_1.databinding.LibraryItemsBinding
+import interfaces.OnItemClickListener
 
 
-class ItemsAdapter(private val context: Context): RecyclerView.Adapter<ItemsViewHolder>() {
+class ItemsAdapter(
+    private val listener: OnItemClickListener
+): RecyclerView.Adapter<ItemsViewHolder>() {
 
     private val libList = mutableListOf<LibraryItems>()
 
     fun setNewLibList(newLibList: List<LibraryItems>) {
+        libList.clear()
         libList.addAll(newLibList)
         notifyDataSetChanged()
     }
 
-    fun deleteLibListItem(currentData: List<LibraryItems>, position: Int) {
+    fun deleteLibListItem(position: Int) {
         libList.removeAt(position)
         notifyItemRemoved(position)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewHolder {
-        val view = LibraryItemsBinding.inflate((LayoutInflater.from(parent.context)))
-        return ItemsViewHolder(view, context)
+        val view = LibraryItemsBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false
+        )
+        return ItemsViewHolder(view, listener)
+    }
+
+    override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
+        holder.bind(libList[position])
     }
 
     override fun getItemCount(): Int {
         return libList.size
     }
 
-    override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
-        holder.bind(libList[position])
-    }
+
 }
+
+//class DiffCallback : DiffUtil.ItemCallback<LibraryItems>() {
+//    override fun areItemsTheSame(oldItem: LibraryItems, newItem: LibraryItems): Boolean {
+//        return oldItem.id == newItem.id
+//    }
+//
+//    override fun areContentsTheSame(oldItem: LibraryItems, newItem: LibraryItems): Boolean {
+//        return oldItem == newItem
+//    }
+//
+//}
